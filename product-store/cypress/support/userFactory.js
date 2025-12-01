d02cd25cbecfd47f656a55b32bb046d80c3929fb
cordolla10@gmail.com <cordolla10@gmail.com>
2025-12-01 00:05:28 -0300

    Configura gitignore para node_modules

--- product-store/cypress/support/userFactory.js
+++
@@ -1,8 +0,0 @@
-export const generateUser = () => {
-    const timeStamp = Math.floor(Date.now() / 10000);
-    const id = timeStamp.toString().slice(-5)
-    return {
-        username: `Teste${id}`,
-        password: 'SenhaValida'
-    }
-};
